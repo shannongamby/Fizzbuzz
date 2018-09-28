5a590ec7f5a5be19f8780468010602367c3b34e7
Shannon Gamby <shannongamby@hotmail.com>
2018-09-28 15:24:47 +0100

    Add 3rd test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -11,3 +11,9 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(4)).to eq 4
   end
 end
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+end
